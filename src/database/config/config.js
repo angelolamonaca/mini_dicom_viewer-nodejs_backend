18f4a531a4563463755e1f1ba91cfd6155cd4fc6
Angelo <angelo.lamonaca@gmail.com>
2021-12-13 06:52:52 +0100

    Configure sequelize

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,12 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: 'root',
+    password: 'angelo_lamonaca',
+    database: 'dicom-viewer',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    use_env_variable: 'jdbc:mysql://localhost:3308/dicom-viewer',
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+};
